13529d4ebbeef175b6863bb3c0fc647d76abb779
joe-newm <j.newm500@gmail.com>
2023-12-28 18:17:23 -0600

    added functionality to to-do list and delete button

--- main.js
+++ main.js
@@ -11,12 +11,44 @@ const displayCount = (taskCount) => {
 }
 
 const addTask = () => {
-    const taskName = newTaskInput.ariaValueMax.trim()
+    const taskName = newTaskInput.value.trim()
     error.style.display = "none";
     if (!taskName) {
         setTimeout(() => {
             error.style.display = "block";
         }, 200)
+        return;
+    } 
+
+    const task = `<div class="task">
+    <input type="checkbox" class="task-check">
+    <span class="taskname">${taskName}</span>
+    <div class="btn-container">
+    <button class="edit">edit</button>
+    <button class="delete">delete</button>
+    </div>
+    </div>`
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+    taskCount += 1;
+    displayCount(taskCount)
+
+    const delBtn = tasksContainer.lastElementChild.querySelector(".delete")
+    delBtn.addEventListener("click", deleteTask)
+
+    newTaskInput.value = ""
+}
+addBtn.addEventListener("click", addTask)
+
+const deleteTask = (event) => {
+    const task = event.target.closest(".task");
+
+    if (task) {
+        tasksContainer.removeChild(task);
+        taskCount -= 1;
+        displayCount(taskCount);
     }
 }
+
+
 
