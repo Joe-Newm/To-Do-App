fb969aacc31fa166894ea292a41aa0e0875a16b6
joe-newm <j.newm500@gmail.com>
2024-01-01 16:33:00 -0600

    fixed edit button not saving correctly to local storage

--- task.js
+++ task.js
@@ -4,26 +4,41 @@ const tasksContainer = document.querySelector("#tasks");
 const error = document.querySelector("#error");
 const countValue = document.querySelector(".count-value");
 const plural = document.querySelector(".plural-value");
+let taskDiv = document.querySelectorAll(".task");
+let taskarray = Array.from(taskDiv);
+let taskHTMLArray = taskarray.map(taskDiv => taskDiv.innerHTML )
+
 let storedtasks = JSON.parse(localStorage.getItem("storage"));
-let taskItemStorage = [];
+let taskItemStorage = []
 let taskCount = taskItemStorage.length;
 
 // call when adding tasks to save to local storage
 const saveToLocalStorage = () => {
+  gatherTasks();
   localStorage.setItem("storage", JSON.stringify(taskItemStorage));
 };
 
-// attach delete button event listener to added task
+// function to gather task divs
+const gatherTasks = () => {
+  localStorage.removeItem("storage")
+  taskItemStorage = []
+  let taskDiv = document.querySelectorAll(".task");
+  let taskarray = Array.from(taskDiv);
+  taskarray.forEach((taskDiv) => {
+    taskItemStorage.push(taskDiv.outerHTML)
+  })
+}
+
+// function that attches edit and delete button event listener to added task
 const attachEventListeners = () => {
   const delBtn = document.querySelectorAll(".delete");
   delBtn.forEach((button, index) => {
     button.onclick = () => {
       taskItemStorage.splice(index, 1);
       displayCount(taskCount);
-      saveToLocalStorage();
       button.parentElement.parentElement.remove();
-      updateTaskCount();
-      attachEventListeners();
+      saveToLocalStorage();
+      updateTaskCount();  
     };
   });
   // adds the event listener for the edit buttons on each task
@@ -35,7 +50,14 @@ const attachEventListeners = () => {
       inputElement.type = "text";
       inputElement.value = text.innerText;
       text.parentNode.replaceChild(inputElement, text);
-      console.log("hey");
+      inputElement.focus()
+      inputElement.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+          text.innerText = inputElement.value;
+          inputElement.parentNode.replaceChild(text, inputElement);
+          saveToLocalStorage();
+        }
+      })
     };
   });
 };
@@ -47,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   tasksContainer.insertAdjacentHTML("beforeend", taskItemStorage.join(""));
   attachEventListeners();
-  displayCount(taskCount);
+  updateTaskCount()
 });
 
 // changes the plural form of the word task
@@ -78,17 +100,16 @@ const addTask = () => {
   }
 
   const taskHTML = `<div class="task">
-    <input type="checkbox" class="task-check">
-    <span class="taskname">${taskName}</span>
-    <div class="btn-container">
-    <button class="edit">edit</button>
-    <button class="delete">delete</button>
-    </div>
+        <input type="checkbox" class="task-check">
+        <span class="taskname">${taskName}</span>
+        <div class="btn-container">
+            <button class="edit">edit</button>
+            <button class="delete">delete</button>
+        </div>
     </div>`;
 
   tasksContainer.insertAdjacentHTML("beforeend", taskHTML);
   newTaskInput.value = "";
-  taskItemStorage.push(taskHTML);
   updateTaskCount();
   attachEventListeners();
   saveToLocalStorage();
